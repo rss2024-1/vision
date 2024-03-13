68dd9226a980219876113387c224f60126892fdf
evansbaj <evans.benjamin.aj@gmail.com>
2024-03-13 17:31:07 -0400

    Add reverse/retry functionality

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -16,20 +16,11 @@ class ParkingController(Node):
     def __init__(self):
         super().__init__("parking_controller")
 
-        # self.declare_parameter("drive_topic")
-        self.declare_parameters(
-            namespace='', # TODO confirm empty namespace is ok
-            parameters=[
-                ("drive_topic")
-                ("Kp_steer", -1) # TODO pick better PD gains, confirm signs
-                ("Kd_steer", -1)
-                ("Kp_speed", -1)
-                ("Kp_speed", -1)
-                ("cone_dt", 0.02) # timestep between published cone locations; value found from lab3 data - TODO confirm for lab4
-                ("parking_distance", 0.75)
-            ]
-        )
+        self.declare_parameter("drive_topic")
+        self.declare_parameter("cone_dt", 0.02) # timestep between published cone locations; value found from lab3 data - TODO confirm for lab4
+        
         DRIVE_TOPIC = self.get_parameter("drive_topic").value # set in launch file; different for simulator vs racecar
+        self.timestep = self.get_parameter("cone_dt").value
         
         self.drive_pub = self.create_publisher(AckermannDriveStamped, DRIVE_TOPIC, 10)
         self.error_pub = self.create_publisher(ParkingError, "/parking_error", 10)
@@ -37,34 +28,63 @@ class ParkingController(Node):
         self.create_subscription(ConeLocation, "/relative_cone", 
             self.relative_cone_callback, 1)
         
+        # PD gains
+        self.declare_parameter("Kp_steer", -1) # TODO pick better PD gains, confirm signs
+        self.declare_parameter("Kd_steer", -1)
+        self.declare_parameter("Kp_speed", -1)
+        self.declare_parameter("Kp_speed", -1)
         self.Kp_steering = self.get_parameter("Kp_steer").value
         self.Kd_steering = self.get_parameter("Kd_steer").value
         self.Kp_velocity = self.get_parameter("Kp_speed").value
         self.Kd_velocity = self.get_parameter("Kp_speed").value
-        self.timestep = self.get_parameter("cone_dt").value
         
+        # Max/min control values
+        self.declare_parameter("angle_max", 2) # maximum steering angle (left? right?)
+        self.declare_parameter("angle_min", -2) # minimum steering angle
+        self.declare_parameter("speed_max", 10) # maximum forward speed
+        self.declare_parameter("speed_min", -1) # minimum forward speed
+        self.max_steer = self.get_parameter("angle_max").value
+        self.min_steer = self.get_parameter("angle_min").value
+        self.max_speed = self.get_parameter("speed_max").value
+        self.min_speed = self.get_parameter("speed_min").value
+        
+        # Acceptable error values
+        self.declare_parameter("dist_error", 0.01) # acceptable distance error threshold
+        self.declare_parameter("angle_error", 0.0) # acceptable angle error threshold
+        self.dist_error = self.get_parameter("dist_error").value
+        self.angle_error = self.get_parameter("angle_error").value
+        
+        # Reversing/reparking
+        self.reverse_state = False # track whether car is currently backing up
+        self.declare_parameter("steer_rev", -0.5) # multiplier for steering commands while reversing
+        self.rev_steer = self.get_parameter("steer_rev").value
+        self.parking_retry = False # track special state in which car is backing up to try parking again
+        self.park_fail_count = 0
+        self.declare_parameter("fail_count", 50) # number of "failed" parking values to trigger a retry
+        self.declare_parameter("retry_dist", 0.5) # amount to back up when doing a parking retry
+        self.fail_count_max = self.get_parameter("fail_count").value
+        self.park_retry_dist = self.get_parameter("retry_dist").value
+        
+        # Goal parking distance, current coordinates
+        self.declare_parameter("parking_distance", 0.75) # goal distance away from the cone to park 
         self.parking_distance = self.get_parameter("parking_distance").value # default 0.75 meters; try playing with this number!
         self.relative_x = 0
         self.relative_y = 0
-
+        
         self.get_logger().info("Parking Controller Initialized")
         
     def angle_2(x, y):
-        return np.arctan2(y/x) # TODO confirm sign of angle is correct
+        return np.arctan2(y, x) # TODO confirm sign of angle is correct
     
     def distance_2(x, y):
         return np.sqrt(x**2 + y**2)
 
-    def parking_PD_control(self, angle, d_angle, distance, d_distance):
-        # Steering
-        angle_control = self.Kp_steering*angle + self.Kd_steering*d_angle
-        
-        # Velocity
-        dist_error = distance - self.parking_distance
-        speed_control = self.Kp_velocity*dist_error + self.Kd_velocity*d_distance
-        
-        return angle_control, speed_control
-    
+    def speed_PD(self, angle, d_angle):
+        return self.Kp_steering*angle + self.Kd_steering*d_angle
+
+    def steer_PD(self, distance, d_distance):
+        if self.reverse_state: return self.rev_steer * (self.Kp_velocity*distance + self.Kd_velocity*d_distance)
+        else: return self.Kp_velocity*distance + self.Kd_velocity*d_distance
 
     def relative_cone_callback(self, msg):
         prev_angl = self.angle_2(self.relative_x, self.relative_y)
@@ -78,18 +98,42 @@ class ParkingController(Node):
         # YOUR CODE HERE
         # Use relative position and your control law to set drive_cmd
         
-        angl2cone = np.arctan2(self.relative_y/self.relative_x)
+        # Calculate error
+        angl2cone = self.angle_2(self.relative_x, self.relative_y)
         angl_delta = angl2cone - prev_angl
-        dist2cone = np.sqrt((self.relative_y)**2 + (self.relative_x)**2)
+        dist2cone = self.distance_2(self.relative_x, self.relative_x) - self.parking_distance # actually distance to target point, not exactly cone
         dist_delta = dist2cone - prev_dist
         
-        steering_angle, forward_speed = self.parking_PD_control(angl2cone, angl_delta/self.timestep,
-                                                                dist2cone, dist_delta/self.timestep)
+        # Check if parking retry conditions met repeatedly
+        if abs(dist2cone) < self.dist_error and abs(angl2cone) > self.angle_error:
+            self.park_fail_count += 1
+            if self.park_fail_count > self.fail_count_max:
+                self.parking_retry = True
+                self.park_fail_count = 0
+        
+        elif self.park_fail_count > 0 and not self.parking_retry:
+            self.park_fail_count -= 1
+        
+        # Calculate appropriate speed
+        speed = np.clip(self.speed_PD(dist2cone, dist_delta/self.timestep), self.min_speed, self.max_speed)
+        
+        # Logic for parking retry state
+        if self.parking_retry == True:
+            speed = self.min_speed
+            if dist2cone >= self.park_retry_dist:
+                self.parking_retry = False
+        
+        # Check if car is backing up
+        self.reverse_state = (speed < 0.0)
+        
+        # Calculate appropriate steering angle
+        steer = np.clip(self.steer_PD(angl2cone, angl_delta/self.timestep), self.min_steer, self.max_steer)
+            
         
         drive_cmd.header.stamp = self.get_clock().now().to_msg()
-        drive_cmd.header.frame_id = "robot?" # TODO set correct frame id
-        drive_cmd.drive.steering_angle = steering_angle
-        drive_cmd.drive.speed = max(0, forward_speed)
+        drive_cmd.header.frame_id = "/map" # TODO set correct frame id
+        drive_cmd.drive.steering_angle = steer
+        drive_cmd.drive.speed = speed
 
         #################################
 
