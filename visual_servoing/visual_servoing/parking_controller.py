b35344d7df2e60b4d2051800dd0c73322f9bc740
eman-ant <eanteneh3@gmail.com>
2024-03-13 21:17:05 +0000

    Working forward parking controller (tuning needed)

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -16,22 +16,56 @@ class ParkingController(Node):
     def __init__(self):
         super().__init__("parking_controller")
 
-        self.declare_parameter("drive_topic")
-        DRIVE_TOPIC = self.get_parameter("drive_topic").value # set in launch file; different for simulator vs racecar
+        self.declare_parameter("drive_topic", "default")
+        self.declare_parameter("Kp_steer", 1.0)
+        self.declare_parameter("Kd_steer", 1.0)
+        self.declare_parameter("Kd_speed", 1.0)
+        self.declare_parameter("Kp_speed", 1.0)
+        self.declare_parameter("cone_dt", 0.02)
+        self.declare_parameter("parking_distance", 0.75)
 
-        self.drive_pub = self.create_publisher(AckermannDriveStamped, DRIVE_TOPIC, 10)
+        self.DRIVE_TOPIC = self.get_parameter("drive_topic").get_parameter_value().string_value # set in launch file; different for simulator vs racecar
+        self.DRIVE_TOPIC = "/drive"    
+    
+        self.drive_pub = self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 10)
         self.error_pub = self.create_publisher(ParkingError, "/parking_error", 10)
 
         self.create_subscription(ConeLocation, "/relative_cone", 
             self.relative_cone_callback, 1)
-
-        self.parking_distance = .75 # meters; try playing with this number!
+        
+        self.Kp_steering = self.get_parameter("Kp_steer").value
+        self.Kd_steering = self.get_parameter("Kd_steer").value
+        self.Kp_velocity = self.get_parameter("Kp_speed").value
+        self.Kd_velocity = self.get_parameter("Kd_speed").value
+        self.timestep = self.get_parameter("cone_dt").value
+        
+        self.parking_distance = self.get_parameter("parking_distance").value # default 0.75 meters; try playing with this number!
         self.relative_x = 0
         self.relative_y = 0
 
         self.get_logger().info("Parking Controller Initialized")
+        
+    def angle_2(self, x, y):
+        return np.arctan2(y, x) # TODO confirm sign of angle is correct
+    
+    def distance_2(self, x, y):
+        return np.sqrt(x**2 + y**2)
+
+    def parking_PD_control(self, angle, d_angle, distance, d_distance):
+        # Steering
+        angle_control = self.Kp_steering*angle + self.Kd_steering*d_angle
+        
+        # Velocity
+        dist_error = distance - self.parking_distance
+        speed_control = self.Kp_velocity*dist_error + self.Kd_velocity*d_distance
+        
+        return angle_control, speed_control
+    
 
     def relative_cone_callback(self, msg):
+        print("cone callback called")
+        prev_angl = self.angle_2(self.relative_x, self.relative_y)
+        prev_dist = self.distance_2(self.relative_x, self.relative_y)
         self.relative_x = msg.x_pos
         self.relative_y = msg.y_pos
         drive_cmd = AckermannDriveStamped()
@@ -40,9 +74,24 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Use relative position and your control law to set drive_cmd
+        
+        angl2cone = self.angle_2(self.relative_x, self.relative_y)
+        angl_delta = angl2cone - prev_angl
+        dist2cone = self.distance_2(self.relative_x, self.relative_y)
+        dist_delta = dist2cone - prev_dist
+        print(angl2cone, dist2cone)
+        
+        steering_angle, forward_speed = self.parking_PD_control(angl2cone, angl_delta/self.timestep,
+                                                                dist2cone, dist_delta/self.timestep)
+        
+        print(steering_angle, forward_speed)
+        forward_speed = min(forward_speed, 1.0)
+        drive_cmd.header.stamp = self.get_clock().now().to_msg()
+        drive_cmd.header.frame_id = "/map" # TODO set correct frame id
+        drive_cmd.drive.steering_angle = steering_angle
+        drive_cmd.drive.speed = -1*max(0.0, forward_speed)
 
         #################################
-
         self.drive_pub.publish(drive_cmd)
         self.error_publisher()
 
@@ -57,6 +106,10 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
+        x, y = self.relative_x, self.relative_y
+        error_msg.x_error = x
+        error_msg.y_error = y
+        error_msg.distance_error = self.distance_2(x, y)
 
         #################################
         
